8810826543388c81626891c219c13b4e6e01116d
Magnus Lundvall <lundvall@gmail.com>
2022-12-25 03:02:37 +0100

    Add warmup for onvif parser.

--- src/main/java/be/teletask/onvif/parsers/DiscoveryParser.java
+++ src/main/java/be/teletask/onvif/parsers/DiscoveryParser.java
@@ -30,6 +30,41 @@ public class DiscoveryParser extends OnvifParser<List<Device>> {
     private static final String KEY_UPNP_USN = "USN: ";
     private static final String KEY_UPNP_ST = "ST: ";
 
+    private static final String WARMUP_XML = "" +
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\"\n" +
+            "                   xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\"\n" +
+            "                   xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\"\n" +
+            "                   xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"\n" +
+            "                   xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\"\n" +
+            "                   xmlns:tds=\"http://www.onvif.org/ver10/device/wsdl\">\n" +
+            "<SOAP-ENV:Header>\n" +
+            "    <wsa:MessageID>uuid:846a7224-7a5d-4b2d-9f0f-d6ab3cea6ce7</wsa:MessageID>\n" +
+            "    <wsa:RelatesTo>uuid:bd64d03b-c701-4b50-a4bc-18c4048971c3</wsa:RelatesTo>\n" +
+            "    <wsa:To SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>\n" +
+            "    <wsa:Action SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2005/04/discovery/ProbeMatches</wsa:Action>\n" +
+            "    <d:AppSequence SOAP-ENV:mustUnderstand=\"true\" MessageNumber=\"175811\" InstanceId=\"1670367710\"></d:AppSequence>\n" +
+            "</SOAP-ENV:Header>\n" +
+            "<SOAP-ENV:Body>\n" +
+            "    <d:ProbeMatches>\n" +
+            "        <d:ProbeMatch>\n" +
+            "            <wsa:EndpointReference>\n" +
+            "                <wsa:Address>urn:uuid:e83a0086-3fdc-4d87-b8c4-83c0a16ad9f4</wsa:Address>\n" +
+            "            </wsa:EndpointReference>\n" +
+            "            <d:Types>tds:Device dn:NetworkVideoTransmitter</d:Types>\n" +
+            "            <d:Scopes>onvif://www.onvif.org/Profile/Streaming onvif://www.onvif.org/Profile/G\n" +
+            "                onvif://www.onvif.org/hardware/M2036-LE onvif://www.onvif.org/name/AXIS%20M2036-LE\n" +
+            "                onvif://www.onvif.org/Profile/M onvif://www.onvif.org/Profile/T onvif://www.onvif.org/location/\n" +
+            "            </d:Scopes>\n" +
+            "            <d:XAddrs>http://192.168.0.90/onvif/device_service https://192.168.0.90/onvif/device_service\n" +
+            "                http://169.254.209.66/onvif/device_service https://169.254.209.66/onvif/device_service\n" +
+            "            </d:XAddrs>\n" +
+            "            <d:MetadataVersion>1</d:MetadataVersion>\n" +
+            "        </d:ProbeMatch>\n" +
+            "    </d:ProbeMatches>\n" +
+            "</SOAP-ENV:Body>\n" +
+            "</SOAP-ENV:Envelope>";
+
     //Attributes
     private DiscoveryMode mode;
     private String hostName;
@@ -38,6 +73,9 @@ public class DiscoveryParser extends OnvifParser<List<Device>> {
     public DiscoveryParser(DiscoveryMode mode) {
         this.mode = mode;
         hostName = "";
+
+        // warmup the parser
+        parse(new OnvifResponse<>(WARMUP_XML));
     }
 
     //Methods
